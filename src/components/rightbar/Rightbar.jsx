69910de8898355a4f96d2cbe37f25ab82120b1b8
yamankubra <ymn.kubra@gmail.com>
2022-05-06 17:28:26 +0300

    created rightbar

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -3,7 +3,74 @@ import "./rightbar.css";
 export default function Rightbar() {
   return (
     <div className="rightbar">
-        Rightbar
+        <div className="rightbarWrapper">
+          <div className="birthdayContainer">
+            <img className="birthdayImg" src="assets/gift.png" alt="" />
+            <span className="birthdayText">
+              <b>Pola Foster</b> and <b>3 Other friends</b> have a birthday
+            </span>
+          </div>
+          <img src="assets/ad.jpg" alt="" className="rightbarAd" />
+          <h4 className="rightbarTitle">Online Friends</h4>
+          <ul className="rightbarFriendList">
+            <li className="rightbarFriend">
+              <div className="rightbarProfileImgContainer">
+                <img src="assets/person/3.jpg" alt="" className="rightbarProfileImg" />
+                <span className="rightbarOnline"></span>
+              </div>
+              <span className="rightbarUsername">James Austen</span>
+            </li>
+            <li className="rightbarFriend">
+              <div className="rightbarProfileImgContainer">
+                <img src="assets/person/3.jpg" alt="" className="rightbarProfileImg" />
+                <span className="rightbarOnline"></span>
+              </div>
+              <span className="rightbarUsername">James Austen</span>
+            </li>
+            <li className="rightbarFriend">
+              <div className="rightbarProfileImgContainer">
+                <img src="assets/person/3.jpg" alt="" className="rightbarProfileImg" />
+                <span className="rightbarOnline"></span>
+              </div>
+              <span className="rightbarUsername">James Austen</span>
+            </li>
+            <li className="rightbarFriend">
+              <div className="rightbarProfileImgContainer">
+                <img src="assets/person/3.jpg" alt="" className="rightbarProfileImg" />
+                <span className="rightbarOnline"></span>
+              </div>
+              <span className="rightbarUsername">James Austen</span>
+            </li>
+            <li className="rightbarFriend">
+              <div className="rightbarProfileImgContainer">
+                <img src="assets/person/3.jpg" alt="" className="rightbarProfileImg" />
+                <span className="rightbarOnline"></span>
+              </div>
+              <span className="rightbarUsername">James Austen</span>
+            </li>
+            <li className="rightbarFriend">
+              <div className="rightbarProfileImgContainer">
+                <img src="assets/person/3.jpg" alt="" className="rightbarProfileImg" />
+                <span className="rightbarOnline"></span>
+              </div>
+              <span className="rightbarUsername">James Austen</span>
+            </li>
+            <li className="rightbarFriend">
+              <div className="rightbarProfileImgContainer">
+                <img src="assets/person/3.jpg" alt="" className="rightbarProfileImg" />
+                <span className="rightbarOnline"></span>
+              </div>
+              <span className="rightbarUsername">James Austen</span>
+            </li>
+            <li className="rightbarFriend">
+              <div className="rightbarProfileImgContainer">
+                <img src="assets/person/3.jpg" alt="" className="rightbarProfileImg" />
+                <span className="rightbarOnline"></span>
+              </div>
+              <span className="rightbarUsername">James Austen</span>
+            </li>
+          </ul>
+        </div>
     </div>
   )
 }
